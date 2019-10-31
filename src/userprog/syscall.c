7ac95038b6248025ebcac3466e628b6bead39816
CHOIWOOSEOK <21500740@handong.edu>
2019-10-31 12:59:44 +0900

    Problem 2-2

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -9,7 +9,9 @@ static void syscall_handler (struct intr_frame *);
 void
 syscall_init (void) 
 {
+printf("syscall_init START!\n");
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+printf("syscall_init END!\n");
 }
 
 static void
