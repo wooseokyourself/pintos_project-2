d75fab2160cdf1653515221a2dd7d0bd3c77821c
CHOIWOOSEOK <21500740@handong.edu>
2019-11-06 04:24:03 +0900

    Problem 2-3

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -149,14 +149,14 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
-/*
+
   // ASSERT (!is_kernel_vaddr (fault_addr)); << 이렇게 하면 안됨. 아래처럼 해야함.
   if (!user || is_kernel_vaddr (fault_addr)) // 이부분은 블로그 코드를 참고하였다!
   {
 //printf(" >> in exception.c detects that user program refers to kernel area!\n");
      exit(-1);
   }
-*/
+
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
